1732743f90bbf9754305678f83d032c4bf36f250
Aqeel <aqeel@gmail.com>
2026-09-12 21:00:24 +0500

    first commit

---
+++ apps/school_management/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+import uuid
+# Create your models here.
+
+class student(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
