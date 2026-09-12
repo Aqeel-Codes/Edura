054798e12465b17216e2a25d337b3b8ab33d6cb9
Aqeel <aqeel@gmail.com>
2026-09-12 21:26:38 +0500

    Moved apps folder inside the Django project directory

--- apps/school_management/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-import uuid
-# Create your models here.
-
-class student(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
-    email = models.EmailField(unique=True)
